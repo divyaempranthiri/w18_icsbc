99b22f5699438cbbb24baeea7c104c9325b4fe0e
divyaempranthiri <divya.empranthiri@berkeley.edu>
2019-01-03 19:18:09 -0800

    Completed Table of Contents Assignment

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,9 +1,13 @@
 # Make sure you read the whole tip section, there's some good stuff there!
 
-line_width = # Set it to a default length you'll use throughout
+line_width = 60 # Set it to a default length you'll use throughout
 title = "Table of Contents"
 
 # Depending on your solution, you may use more than one array, but here's one to get you started
-chapters = [] 
-
+chapters = [["Getting Started", "1"], ["Numbers", "9"], ["Letters", "13"]]
 ### Your Code Here ###
+
+puts title.center(line_width)
+chapters.each_with_index do |chapterinfo, index|
+  puts ("Chapter " + (index+1).to_s + ": " + chapterinfo[0]).ljust(line_width/2) + ("page " + chapterinfo[1]).rjust(line_width/2)
+end
